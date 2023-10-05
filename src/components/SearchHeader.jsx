5af3099a720c33e009a3f8f5737c3f2be071d668
Taegwan Youn <tkyoun0421@naver.com>
2023-10-05 22:19:25 +0900

    Feat: Add tailwindcss

---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { BsYoutube, BsSearch } from 'react-icons/bs';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+export default function SearchHeader() {
+  const { keyword } = useParams();
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+
+  useEffect(() => setText(keyword || ''), [keyword]);
+
+  return (
+    <header className="w-full flex p-4 text-2xl border-b border-zinc-600 mb-4">
+      <Link to="/" className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
+      </Link>
+      <form className="w-full flex justify-center" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
+}
