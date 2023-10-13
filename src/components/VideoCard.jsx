efffb20fbd1f7aa5efd09d713869155b75a1e8d7
Taegwan Youn <tkyoun0421@naver.com>
2023-10-13 14:11:39 +0900

    Feat: Add relatedvideos

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideoCard({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
   return (
-    <li>
-      <img src={thumbnails.medium.url} alt={title}></img>
+    <li
+      onClick={() => {
+        navigate(`videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img className={'w-full'} src={thumbnails.medium.url} alt={title}></img>
       <div>
-        <p>{title}</p>
-        <p>{channelTitle}</p>
-        <p>{publishedAt}</p>
+        <p className={'font-semibold my-2 line-clamp-2'}>{title}</p>
+        <p className={'text-sm opacity-80'}>{channelTitle}</p>
+        <p className={'text-sm opacity-80'}>{formatAgo(publishedAt, 'ko')}</p>
       </div>
     </li>
   );
