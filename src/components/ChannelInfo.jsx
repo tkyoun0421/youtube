475d58e0fd4edf45f298400387f933a26015d61f
Taegwan Youn <tkyoun0421@naver.com>
2023-10-13 14:26:42 +0900

    Feat: Add relatedVideo

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from 'react-query';
 
 export default function ChannelInfo({ id, name }) {
-  return <div>{name}</div>;
+  const { youtube } = useYoutubeApi();
+  const { data: url } = useQuery(['channel', id], () =>
+    youtube.channelImageUrl(id)
+  );
+  return (
+    <div>
+      {url && <img src={url} alt={name}></img>}
+      <p>{name}</p>
+    </div>
+  );
 }
--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from 'react-query';
+import VideoCard from './VideoCard';
 
 export default function RelatedVideos({ id }) {
-  return <div>{id}</div>;
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['related', id], () => youtube.relatedVideos(id));
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul
+          className={
+            'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'
+          }
+        >
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
