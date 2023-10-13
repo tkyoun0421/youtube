1a445e4c35382ab04c1e2c92898b212e2fe1f763
Taegwan Youn <tkyoun0421@naver.com>
2023-10-13 14:59:10 +0900

    스타일 완성

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -15,13 +15,9 @@ export default function RelatedVideos({ id }) {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong</p>}
       {videos && (
-        <ul
-          className={
-            'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'
-          }
-        >
+        <ul>
           {videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
+            <VideoCard key={video.id} video={video} type="list" />
           ))}
         </ul>
       )}
